94612043f92b157e2468350be520be510c8252ca
revan jeevanagi <55836074+revanhj@users.noreply.github.com>
2026-06-07 17:45:09 +0530

    removed security

--- src/main/java/com/farmermarket/controller/FarmerController.java
+++ src/main/java/com/farmermarket/controller/FarmerController.java
@@ -3,7 +3,6 @@ package com.farmermarket.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +15,8 @@ public class FarmerController {
 	/**
 	 * Get Farmer By Id
 	 */
-	@GetMapping("/{farmerId}")
-	public ResponseEntity<String> getFarmerById(@PathVariable Long farmerId) {
+	@GetMapping("/farmers")
+	public ResponseEntity<String> getFarmerById() {
 
 		String farmerResponse = "Farmer";
 		return ResponseEntity.ok(farmerResponse);
